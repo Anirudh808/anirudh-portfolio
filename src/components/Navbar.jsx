d4d722a6bd755407c0ff6718349a0f3e8196bbde
Anirudh <anirudhmounasamy@gmail.com>
2025-11-21 23:34:35 +0530

    implements navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import dayjs from "dayjs";
+
+import { navIcons, navLinks } from "#constants";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" />
+        <p className="font-bold">Anirudh's Portfolio</p>
+
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <p>{link.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img
+                src={img}
+                alt={`icon-$${img}-${id}`}
+                className="icon-hover"
+              />
+            </li>
+          ))}
+        </ul>
+
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
